c37e35693716271bee74d59676b0a037ca054b11
Neon <vanyagame12@gmail.com>
2025-05-14 19:20:07 +0300

    thanks, it works

--- app/src/main/java/com/example/hw_qr_code/MainActivity.kt
+++ app/src/main/java/com/example/hw_qr_code/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.hw_qr_code
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.Volley
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +23,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         imageView = findViewById(R.id.imageView);
-        imageUrl = ""
-
+        imageUrl = "https://raw.githubusercontent.com/Neonchik4/HW-QR-Code/refs/heads/master/app/src/main/res/images/qr-code-image.jpg"
+        val imageRequest = ImageRequest(
+            imageUrl,
+            { response: Bitmap ->
+                imageView.setImageBitmap(response)
+            },
+            0,
+            0,
+            null,
+            Bitmap.Config.ARGB_8888,
+            { _: VolleyError ->
+                Toast.makeText(this@MainActivity, "Изображение не загружено", Toast.LENGTH_SHORT).show()            }
+        )
+        Volley.newRequestQueue(this).add(imageRequest)
     }
 }
