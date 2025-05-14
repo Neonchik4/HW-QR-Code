b39bca29b5fef1d846cea2fb3580cc732a5267c7
Neon <vanyagame12@gmail.com>
2025-05-14 18:36:25 +0300

    initial commit 1

---
+++ app/src/main/java/com/example/hw_qr_code/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hw_qr_code
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private var imageUrl: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView);
+        imageUrl = ""
+
+    }
+}
