cbb49c91666b5015e1f10ce510746358f13f6715
Neon <vanyagame12@gmail.com>
2025-05-14 19:54:50 +0300

    +glide

--- app/src/main/java/com/example/hw_qr_code/MainActivity.kt
+++ app/src/main/java/com/example/hw_qr_code/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.hw_qr_code
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.ImageRequest
-import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,20 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageView = findViewById(R.id.imageView);
-        imageUrl = "https://raw.githubusercontent.com/Neonchik4/HW-QR-Code/refs/heads/master/app/src/main/res/images/qr-code-image.jpg"
-        val imageRequest = ImageRequest(
-            imageUrl,
-            { response: Bitmap ->
-                imageView.setImageBitmap(response)
-            },
-            0,
-            0,
-            null,
-            Bitmap.Config.ARGB_8888,
-            { _: VolleyError ->
-                Toast.makeText(this@MainActivity, "Изображение не загружено", Toast.LENGTH_SHORT).show()            }
-        )
-        Volley.newRequestQueue(this).add(imageRequest)
+        imageView = findViewById(R.id.imageView)
+        imageUrl = "https://raw.githubusercontent.com/Neonchik4/HW-QR-Code/refs/heads/master/app/src/main/res/drawable/images/qr-code-image.jpg"
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)    // кэшировать везде (память и диск)
+            .into(imageView)
+            .clearOnDetach()                              // очищать запрос при отсоединении Activity
     }
 }
